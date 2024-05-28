4b21465dbc7335f38d8ffaa7e1cb81ed53ff8203
RobTobEditor <miroslav.rybin.mx@gmail.com>
2024-05-28 12:16:26 -0600

    Create instr.py
    Nothing

---
+++ instr.py
@@ -0,0 +1,33 @@
+
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+txt_hello = 'Heloooooooooo!'
+txt_next = 'Start test'
+txt_instruction = 'You will pass a test for your health' 
+txt_title = 'Main test'
+txt_name = 'Whats your name?'
+txt_hintname = "Your name here"
+txt_hintage = "Your age here"
+txt_test1 = 'Click to do the first task'
+txt_test2 = 'Click to do the second task'
+txt_test3 = 'Click to do the third task'
+txt_sendresults = 'Finish Result'
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = '1'
+txt_starttest2 = '1'
+txt_starttest3 = '1'
+txt_timer = '00:00:00'
+
+txt_age = 'Whats your age?'
+txt_finalwin = 'Beeeeeeey'
+txt_index = 'Index Rufie'
+txt_workheart = 'Booms hart'
+
+txt_res1 = 'Very low. Fast go to the doctor'
+txt_res2 = 'Low. Go to the doctor'
+txt_res3 = 'Normal. Go to the doctor if you want'
+txt_res4 = 'More than normal'
+txt_res5 = 'High'
